35adb2d5fcb20d387cb21608e5289fce455c6ca3
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2026-04-02 18:52:33 +0530

    Prediction Pipeline

--- Student Performance Prediction/setup.py
+++ Student Performance Prediction/setup.py
@@ -17,10 +17,10 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='mlproject',
+name='Student Performance Prediction',
 version='0.0.1',
-author='Krish',
-author_email='krishnaik06@gmail.com',
+author='Pankaj Singh Rawat',
+author_email='qoolphoenix@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
--- Student Performance Prediction/src/utils.py
+++ Student Performance Prediction/src/utils.py
@@ -8,6 +8,7 @@ import dill
 from src.exception import CustomException
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import r2_score
+import pickle
 
 def save_object(file_path, obj):
     try:
@@ -47,6 +48,14 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
             print(f"{model_name} -> Train: {train_score:.4f}, Test: {test_score:.4f}")
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
